f758c5ec2cbdd5dfcaf44c7c55984e5b18eeb8f3
Ayush Jaiswal <ayush45aj@gmail.com>
2024-09-02 21:21:08 +0530

    changes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,31 +1,12 @@
 import { Router } from "express";
-// import { checkUserAuth } from "../middleware";
 import {
-    // userLoginController,
-    // userLogoutController,
-    // userRegistrationController,
-    // changeUserPasswordController,
-    // sendPassWordResetEmailController,
-    // resetPasswordController,
-    // getUserDetailsController,
     verifyOtpController,
     sendOtpController,
 } from "../controllers/auth";
 
 const router = Router();
 
-// Public routes
-// router.post('/signup', userRegistrationController);
-// router.post('/login', userLoginController);
-// router.post('/send-passord-reset-email', sendPassWordResetEmailController);
-// router.post('/reset-password/:id/:token', resetPasswordController);
-
 router.post('/verify-otp',verifyOtpController);
 router.post('/send-otp', sendOtpController);
-
-// Protected routes
-// router.post('/change-password', checkUserAuth, changeUserPasswordController);
-// router.post('/logout', checkUserAuth, userLogoutController);
-// router.get('/user-info', checkUserAuth, getUserDetailsController);
 
 export default router;
